48cadb7b0a429e304755597880e23a99535e864a
SeungWook_Lee <leesw_0924@icloud.com>
2024-09-28 10:20:16 +0900

    init - SpringBoot

---
+++ SpringBoot/src/main/java/com/knu/talentshare/TalentshareApplication.java
@@ -0,0 +1,13 @@
+package com.knu.talentshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentshareApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/src/test/java/com/knu/talentshare/TalentshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.talentshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
